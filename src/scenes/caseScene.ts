2b0f9b2bd2f90f4a8c4d46d55221275d6dd75009
Agatha-Moreira <1agatha.m.f@gmail.com>
2024-06-19 17:19:41 -0300

    Finalização do game

--- src/scenes/caseScene.ts
+++ src/scenes/caseScene.ts
@@ -1,9 +1,11 @@
-import { Actor, Color, Engine, FadeInOut, Keys, Scene, SceneActivationContext, Transition, vec } from "excalibur";
+import { Actor, Color, Engine, FadeInOut, Keys, Scene, SceneActivationContext, Sprite, Transition, vec } from "excalibur";
 import { Resources } from "../resources";
 
 export class caseScene extends Scene {
-    elementoTexto?: HTMLElement
     private objetoInteracao: any
+    private elementoTexto?: HTMLElement
+    private actorFuncionario?: Actor
+    private listaImagens?: Sprite[]
 
     private textoDaCena: string = ""
 
@@ -31,85 +33,78 @@ export class caseScene extends Scene {
 
         // Conteudo dos cases
         this.elementoTexto = document.createElement("div") as HTMLElement
-        this.elementoTexto.style.opacity = "1"
+        this.elementoTexto.classList.add("texto-case")
 
         let containerGame = document.querySelector(".container-game") as HTMLAreaElement
         containerGame.appendChild(this.elementoTexto)
-        this.elementoTexto.classList.add("sobre-gamifica")
 
         this.input.keyboard.on("press", (event) => {
             if (event.key == Keys.R) {
                 this.fadeOutElement(this.elementoTexto!)
-                this.engine.goToScene("expoScene")
+                this.engine.goToScene("Exposicao")
             }
         })
+
+        // Criar Actor para as imagens
+        this.actorFuncionario = new Actor ({
+            pos: vec(engine.drawWidth - 280, engine.halfCanvasWidth - 350)
+        })
+
+        // Carregar imagens
+        let imagemFuncionarioA = Resources.NpcA.toSprite()
+        let imagemFuncionarioB = Resources.NpcB.toSprite()
+        let imagemFuncionarioC = Resources.NpcC.toSprite()
+
+        this.listaImagens = [imagemFuncionarioA, imagemFuncionarioB, imagemFuncionarioC]
     }
 
     onActivate(context: SceneActivationContext<unknown>): void {
         // Pegar dados vindos da cena passada
         this.objetoInteracao = context.data
+        this.elementoTexto!.style.opacity = "1"
 
         // Se for a mesa A
         if (this.objetoInteracao.nomeDoActor == "mesa_stand_a") {
             this.textoDaCena = "Essa é a descrição do case a"
 
-            this.input.keyboard.on("press", (event) => {
-                if (event.key == Keys.R) {
-                    this.fadeOutElement(this.elementoTexto!)
-                    this.engine.goToScene("expoScene")
-                }
-            })
-        }
+            this.elementoTexto!.innerHTML = `<h2>XYZ Tech</h2>
+            <p>Nossa empresa cria soluções de gamificação personalizadas para empresas de todos os tamanhos e setores, usando inteligência artificial e design de jogos para desenvolver estratégias interativas que melhoram a experiência do usuário e impulsionam resultados. Acreditamos no poder dos jogos e da tecnologia para engajar equipes, aumentar a produtividade e motivar, adaptando cada projeto às necessidades específicas do cliente, desde programas de treinamento interativo até sistemas de recompensa e engajamento de funcionários.</p>`
+           
+        // Inserir o sprite do Actor
+        this.actorFuncionario?.graphics.add(this.listaImagens![0])
 
+        // Mudar o zoom da imagem
+        this.actorFuncionario!.graphics.current!.scale = vec(1.9, 1.9)
+        
+
+        }
 
         if (this.objetoInteracao.nomeDoActor == "mesa_stand_b") {
             this.textoDaCena = "Essa é a descrição do case b"
 
-            this.backgroundColor = Color.fromRGB(64, 63, 76)
-            this.elementoTexto = document.createElement("div") as HTMLElement
-            this.elementoTexto.style.opacity = "1"
-            let containerGame = document.querySelector(".container-game") as HTMLAreaElement
-            containerGame.appendChild(this.elementoTexto)
-            this.elementoTexto.classList.add("sobre-gamifica")
-
-            this.elementoTexto.innerHTML = `<h2>Case Escola!</h2>
-            <p>Nossa empresa cria soluções de gamificação personalizadas para empresas de todos os tamanhos e setores,usando inteligência artificial e design de jogos para desenvolver estratégias interativas que melhoram aexperiência do usuário e impulsionam resultados. Acreditamos no poder dos jogos e da tecnologia para engajar equipes, aumentar a produtividade e motivar, adaptando cada projeto às necessidades específicas do cliente,desde programas de treinamento interativo até sistemas de recompensa e engajamento de funcionários.</p>`
-
-
-            this.input.keyboard.on("press", (event) => {
-                if (event.key == Keys.R) {
-                    this.fadeOutElement(this.elementoTexto!)
-                    this.engine.goToScene("expoScene")
-                }
-            })
+            this.elementoTexto!.innerHTML = `<h2>ABC Finance</h2>
+            <p>Nossa empresa cria soluções de gamificação personalizadas para empresas de todos os tamanhos e setores, usando inteligência artificial e design de jogos para desenvolver estratégias interativas que melhoram a experiência do usuário e impulsionam resultados. Acreditamos no poder dos jogos e da tecnologia para engajar equipes, aumentar a produtividade e motivar, adaptando cada projeto às necessidades específicas do cliente,desde programas de treinamento interativo até sistemas de recompensa e engajamento de funcionários.</p>`
+       
+            this.actorFuncionario?.graphics.add(this.listaImagens![1])
+            this.actorFuncionario!.graphics.current!.scale = vec(1.9, 1.9)
 
         }
-
-
 
         if (this.objetoInteracao.nomeDoActor == "mesa_stand_c") {
             this.textoDaCena = "Essa é a descrição do case c"
-            this.backgroundColor = Color.fromRGB(64, 63, 76)
-            this.elementoTexto = document.createElement("div") as HTMLElement
-            this.elementoTexto.style.opacity = "1"
-            let containerGame = document.querySelector(".container-game") as HTMLAreaElement
-            containerGame.appendChild(this.elementoTexto)
-            this.elementoTexto.classList.add("sobre-gamifica")
+       
+            this.elementoTexto!.innerHTML = `<h2>FastMart</h2>
+            <p>Nossa empresa cria soluções de gamificação personalizadas para empresas de todos os tamanhos e setores, usando inteligência artificial e design de jogos para desenvolver estratégias interativas que melhoram a experiência do usuário e impulsionam resultados. Acreditamos no poder dos jogos e da tecnologia para engajar equipes, aumentar a produtividade e motivar, adaptando cada projeto às necessidades específicas do cliente,desde programas de treinamento interativo até sistemas de recompensa e engajamento de funcionários.</p>`
 
-            this.elementoTexto.innerHTML = `<h2>Case Escola!</h2>
-            <p>Nossa empresa cria soluções de gamificação personalizadas para empresas de todos os tamanhos e setores,usando inteligência artificial e design de jogos para desenvolver estratégias interativas que melhoram aexperiência do usuário e impulsionam resultados. Acreditamos no poder dos jogos e da tecnologia para engajar equipes, aumentar a produtividade e motivar, adaptando cada projeto às necessidades específicas do cliente,desde programas de treinamento interativo até sistemas de recompensa e engajamento de funcionários.</p>`
-
-
-            this.input.keyboard.on("press", (event) => {
-                if (event.key == Keys.R) {
-                    this.fadeOutElement(this.elementoTexto!)
-                    this.engine.goToScene("expoScene")
-                }
-            })
+            this.actorFuncionario?.graphics.add(this.listaImagens![2])
+            this.actorFuncionario!.graphics.current!.scale = vec(1.9, 1.9)
         }
+
+        this.add(this.actorFuncionario!)
+
     }
 
     onDeactivate(context: SceneActivationContext<undefined>): void {
-        this.elementoTexto?.remove()
+        this.elementoTexto!.style.opacity = "0"
     }
 }
